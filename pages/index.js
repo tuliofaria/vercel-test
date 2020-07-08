9327728b5ad277c3fd2bbbedd80845a99eaac6a6
Tulio Faria <tuliofaria@gmail.com>
2020-07-08 11:36:15 -0300

    initial.

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Index = ({ domain }) => {
+  return(
+    <div>
+      <h1>Domain: {domain}</h1>
+    </div>
+  )
+}
+export default Index
+
+export async function getServerSideProps ({ res, req }) {
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
+  return {
+    props:{
+      domain: req.headers.host
+    }
+  }
+}
